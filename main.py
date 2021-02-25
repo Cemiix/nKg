5ffb8d868d6f79ef34cd8a8b997c4e85aa5bdf77
Maks   <maksik.ivan4enko@gmail.com>
2021-02-25 18:18:59 +0000

    ....

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-# from flask import Flask
+from flask import Flask
 
-# web = Flask(__name__)
+web = Flask(__name__)
 
-# @web.route("/")
-# def index():
-# 	return "hello"
+@web.route("/")
+def index():
+	return "hello"
 
-# web.run("0.0.0.0")
-
-import os
-
-print(os.system("npm install -g heroku"))
-print(os.system("heroku --version"))
+web.run("0.0.0.0")
