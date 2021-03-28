eed70e8979052239acd2255640a05c3ce6a6f158
Cemiix <60036261+Cemiix@users.noreply.github.com>
2021-03-28 14:36:28 +0300

    Create restart.py

---
+++ modules/system/restart.py
@@ -0,0 +1,13 @@
+from pyrogram import Client, filters
+
+class Main:
+	des = "Модуль для перезагрузки бота"
+	cmd_list = {
+		"restart": "Перезагрузить"
+	}
+	
+@Client.on_message(filters.me & filters.command("restart", "."))
+def restart_bot(app, msg):
+	msg.edit("Перезагрузка...")
+	app.restart(False)
+	msg.edit("Перезагрузка прошла успешно!")
