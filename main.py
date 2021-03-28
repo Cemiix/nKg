9c648cf840d3f857d8c78c7ba2fb6bdcbdef1f88
Cemiix <60036261+Cemiix@users.noreply.github.com>
2021-03-28 14:31:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from pyrogram import Client
+from flask import Flask
+
+web = Flask(__name__)
+@web.route("/")
+def home():
+	return "Hello world"
 
 app = Client("app")
 
+web.run("0.0.0.0")
 app.run()
